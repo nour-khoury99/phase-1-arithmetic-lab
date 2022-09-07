3d7d6a26c4d8287013048722e073dbdadf3a24a0
nour khoury <khourynour79@gmail.com>
2022-09-07 14:35:08 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
+let multiply;
+
+multiply = num1 * num2;
+
+let random;
+random = Math.random() * 10 ;
+
+let mod ;
+mod = num3 % num4;
+
+
+
+let max ;
+max = Math.max(1,3,15,20,14,6,8,7,9);
+ 
 // Write your code here
